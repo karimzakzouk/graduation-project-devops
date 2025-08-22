a51ba0d3f1c338295bab7f1f37ff2c99804f36ac
KarimZakzouk <karimzakzouk69@gmail.com>
2025-08-23 02:40:06 +0300

    Add output for EKS cluster certificate authority data in outputs.tf

--- Terraform/modules/eks/outputs.tf
+++ Terraform/modules/eks/outputs.tf
@@ -7,3 +7,8 @@ output "cluster_name" {
   description = "EKS cluster name"
   value = aws_eks_cluster.main.name
 }
+
+output "cluster_certificate_authority_data" {
+  description = "EKS cluster certificate authority data"
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
