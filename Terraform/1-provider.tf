ce78ad0ed82c54de4c26d2fe096ecfd39ed56ab9
KarimZakzouk <karimzakzouk69@gmail.com>
2025-08-23 02:35:30 +0300

    Refactor GitHub Actions workflows and Terraform configurations; update image tag handling, add ArgoCD installation, and configure providers for Kubernetes and Helm.

--- Terraform/1-provider.tf
+++ Terraform/1-provider.tf
@@ -3,3 +3,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Configure the Kubernetes Provider
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+# Configure the Helm Provider
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
--- Terraform/3-main.tf
+++ Terraform/3-main.tf
@@ -16,5 +16,26 @@ module "eks" {
   vpc_id                = module.vpc.vpc_id
   subnet_ids            = module.vpc.private_subnet_ids
   node_groups           = var.node_groups
+}
 
+# Install ArgoCD
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  
+  create_namespace = true
+  
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+  
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
+
+  depends_on = [module.eks]
 }
