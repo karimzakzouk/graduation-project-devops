b24ad1f941a13128673a18d32347ff837f322a1e
KarimZakzouk <karimzakzouk69@gmail.com>
2025-08-23 07:58:05 +0300

    Update dex_secret_key variable to set default value to null

--- Terraform/modules/argocd/variables.tf
+++ Terraform/modules/argocd/variables.tf
@@ -59,6 +59,6 @@ variable "dex_secret_key" {
   description = "Dex server secret key for ArgoCD (production)"
   type        = string
   sensitive   = true
-  default     = random_password.dex_secret.result
+  default     = null
 }
 
